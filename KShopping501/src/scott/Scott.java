2160478387ef8ccd693b29d91cd6220e0a3f4c85
pvpanime <nemisprint@gmail.com>
2024-11-15 09:10:28 +0900

    save changes for preparing README...

--- KShopping501/src/scott/Scott.java
+++ KShopping501/src/scott/Scott.java
@@ -15,7 +15,6 @@ public class Scott {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
 			System.err.println("이런!! 데이터베이스 클래스를 찾는 데 실패했습니다. 라이브러리를 다시 확인해주세요.\n데이터베이스 관련 기능이 모두 동작하지 않을 것입니다.");
-//			e.printStackTrace();
 		}
 	}
 	
